cb82d6c9d59f742d96116b03ccd8c739df775e90
Chriiis85 <krizmoreno85@gmail.com>
2024-01-17 21:22:31 +0100

    todo terminado

--- script.js
+++ script.js
@@ -1,91 +1,187 @@
 let botones = document.querySelectorAll("button");
+let letrero = document.querySelector(".letrero");
 
-function generarSimbolo() {
-  let ale = Math.floor(Math.random() * 3);
-  switch (ale) {
-    case 0:
-      return "piedra";
-      break;
-    case 1:
-      return "papel";
-      break;
-    case 2:
-      return "tijera";
-      break;
-    default:
-      break;
-  }
-}
+var resultados = [];
+var estaTrampaActivada = Boolean(false);
 
-function simboloMayor(string1, string2) {
-  if (string1 == "piedra" && string2 == "tijera") {
-    return 1;
-  } else if (string2 == "piedra" && string1 == "tijera") {
-    return 2;
-  } else if (string1 == "tijera" && string2 == "papel") {
-    return 1;
-  } else if (string2 == "tijera" && string1 == "papel") {
-    return 2;
-  } else if (string1 == "papel" && string2 == "piedra") {
-    return 1;
-  } else if (string2 == "papel" && string1 == "piedra") {
-    return 2;
-  } else {
-    return 3;
-  }
-}
+for (let boton of botones) {
+  boton.addEventListener("click", function () {
+    let simboloJug = boton.getAttribute("id");
+    //console.log(simboloJug);
 
-function trampa(simbJUg) {
-  let numAle = Math.floor(Math.random() * 10);
-  let simboloIA = generarSimbolo();
-  if (numAle < 10) {
-    if (simbJUg == "piedra") {
-      return "papel";
-    } else if (simbJUg == "papel") {
-      return "tijera";
-    } else if (simbJUg == "tijera") {
-      return "piedra";
-    }
-  }
-}
-
-for (let index = 0; index < botones.length; index++) {
-  botones[index].addEventListener("click", function (b) {
-    let simboloJug = botones[index].getAttribute("id");
     switch (simboloJug) {
-      case "trampa":
-        let simboloIAtrampa = trampa(simboloJug);
-        if (simboloMayor(simboloJug, simboloIAtrampa) == 1) {
-          window.alert("Ha ganado el jugador!");
-          location.reload();
-        } else if (simboloMayor(simboloJug, simboloIAtrampa) == 2) {
-          window.alert("Ha ganado la maquina!");
-          location.reload();
-        } else {
-          window.alert("La partida termina en Empate!");
-          location.reload();
-        }
-        break;
-      case "fin":
-        break;
       case "piedra":
       case "papel":
       case "tijera":
-        let simboloIA = generarSimbolo();
-        console.log(simboloIA);
-        if (simboloMayor(simboloJug, simboloIA) == 1) {
-          window.alert("Ha ganado el jugador!");
-          location.reload();
-        } else if (simboloMayor(simboloJug, simboloIA) == 2) {
-          window.alert("Ha ganado la maquina!");
-          location.reload();
+        gestionarRonda(simboloJug);
+        break;
+      case "trampa":
+        if (!estaTrampaActivada) {
+          estaTrampaActivada = true;
+          console.log("La trampa se ha activado");
+          letrero.textContent = "¡LAS TRAMPAS HAN SIDO ACTIVADAS!";
         } else {
-          window.alert("La partida termina en Empate!");
-          location.reload();
+          estaTrampaActivada = false;
+          console.log("La trampa se ha desactivado");
+          letrero.textContent = "¡LAS TRAMPAS HAN SIDO DESACTIVADAS!";
         }
         break;
+      case "fin":
+        final();
+        break;
+
       default:
         break;
     }
   });
+}
+
+function getSimbolo() {
+  let random = Math.floor(Math.random() * 3);
+  switch (random) {
+    case 0:
+      return "piedra";
+    case 1:
+      return "papel";
+    case 2:
+      return "tijera";
+  }
+}
+
+/**
+ *
+ * @param {Simbolo jugador} simbolo1
+ * @param {Simbolo IA} simbolo2
+ */
+function quienGana(simbolo1, simbolo2) {
+  if (simbolo1 == simbolo2) {
+    letrero.textContent = "¡LA PARTIDA TERMINA EN EMPATE! :)";
+    return 0;
+  }
+
+  if (simbolo1 == "piedra" && simbolo2 == "tijera") {
+    letrero.textContent = "¡HA GANADO EL JUGADOR! :)";
+    return 1;
+  } else if (simbolo1 == "tijera" && simbolo2 == "papel") {
+    letrero.textContent = "¡HA GANADO EL JUGADOR! :)";
+    return 1;
+  } else if (simbolo1 == "papel" && simbolo2 == "piedra") {
+    letrero.textContent = "¡HA GANADO EL JUGADOR! :)";
+    return 1;
+  } else {
+    letrero.textContent = "¡HA GANADO LA MÁQUINA! :(";
+    return 2;
+  }
+}
+
+function gestionarRonda(simboloJugador) {
+  let simboloIA = getSimbolo();
+  if (estaTrampaActivada) {
+    simboloIA = hacerTrampas(simboloJugador);
+  }
+  switch (quienGana(simboloJugador, simboloIA)) {
+    case 0:
+      resultados.push({
+        jugador: simboloJugador,
+        IA: simboloIA,
+        puntos: 0,
+      });
+      break;
+    case 1:
+      resultados.push({
+        jugador: simboloJugador,
+        IA: simboloIA,
+        puntos: 1,
+      });
+      break;
+    case 2:
+      resultados.push({
+        jugador: simboloJugador,
+        IA: simboloIA,
+        puntos: 0,
+      });
+      break;
+
+    default:
+      break;
+  }
+
+  mostrarSimboloDOM(simboloJugador, simboloIA);
+}
+
+function mostrarSimboloDOM(simboloJug, simboloIA) {
+  let resultadoJugador = document.querySelector(".jugador");
+  let resultadoIA = document.querySelector(".IA");
+  resultadoJugador.innerHTML = `<p id="jugador">${dameEmoji(simboloJug)}</p>`;
+  resultadoIA.innerHTML = `<p id="IA">${dameEmoji(simboloIA)}</p>`;
+}
+
+function dameEmoji(simbolo) {
+  switch (simbolo) {
+    case "piedra":
+      return "&#9994;";
+    case "papel":
+      return "&#9995;";
+    case "tijera":
+      return "&#9996;";
+  }
+}
+
+function porcentajeVictorias(partidas, puntos) {
+  return Math.floor((puntos / partidas) * 100);
+}
+
+function final() {
+  let contadorPuntos = 0;
+  let numerorondas = resultados.length;
+
+  for (let resultado of resultados) {
+    if (resultado.puntos == 1) {
+      contadorPuntos++;
+    }
+  }
+  window.alert(
+    "El jugador ha obtenido un total de: " +
+      contadorPuntos +
+      " y su porcentaje de victorias es un total del: " +
+      porcentajeVictorias(numerorondas, contadorPuntos) +
+      "%"
+  );
+  console.log(resultados);
+  location.reload();
+}
+
+function mostrarHistorial(arrayResultados) {
+  for (let i = 0; i < array.length; i++) {}
+}
+
+function hacerTrampas(simboloSelect) {
+  let random = Math.floor(Math.random() * 10);
+  if (random < 8) {
+    return ganarMaquina(simboloSelect);
+  } else {
+    return perderMaquina(simboloSelect);
+  }
+}
+
+function ganarMaquina(simboloJugador) {
+  switch (simboloJugador) {
+    case "piedra":
+      return "papel";
+    case "papel":
+      return "tijera";
+    case "tijera":
+      return "piedra";
+  }
+}
+
+function perderMaquina(simboloJugador) {
+  switch (simboloJugador) {
+    case "piedra":
+      return "tijera";
+    case "papel":
+      return "piedra";
+    case "tijera":
+      return "papel";
+  }
 }
